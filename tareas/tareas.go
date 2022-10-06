7dc05b40bfa83da12c9d5c776979ac2c7cef06d1
Gonzalo <gonzadeveloper@gmail.com>
2022-10-06 12:47:10 -0300

    Pruebas con funciones, punteros y for

--- tareas/tareas.go
+++ tareas/tareas.go
@@ -1,6 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type taskList struct {
+	tasks []*task
+}
+
+func (tl *taskList) agregarALista(t *task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *taskList) eliminarDeLista(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
+func (tl *taskList) imprimirLista() {
+	// no me intereza el indice
+	for _, tarea := range tl.tasks {
+		fmt.Println("Nombre:", tarea.nombre)
+		fmt.Println("Descripción:", tarea.descripcion)
+	}
+}
+
+func (tl *taskList) imprimirListaCompletado() {
+	// no me intereza el indice
+	for _, tarea := range tl.tasks {
+		if tarea.completado {
+			fmt.Println("Nombre:", tarea.nombre)
+			fmt.Println("Descripción:", tarea.descripcion)
+		}
+	}
+}
 
 type task struct {
 	nombre      string
@@ -23,17 +55,59 @@ func (t *task) actualizarNombre(nombre string) {
 
 func main() {
 
-	t := task{
+	t1 := &task{
 		nombre:      "Completar curso",
 		descripcion: "Terminar en esta semana",
 	}
 
-	fmt.Printf("%+v\n", t)
+	t2 := &task{
+		nombre:      "Completar curso Python",
+		descripcion: "Terminar en esta semana Python",
+	}
 
-	t.marcarCompleta()
-	t.actualizarNombre("Terminó el curso")
-	t.actualizarDescripcion("Completé el curso")
+	t3 := &task{
+		nombre:      "Completar curso NodeJS",
+		descripcion: "Terminar en esta semana NodeJS",
+	}
 
-	fmt.Printf("%+v\n", t)
+	lista := &taskList{
+		tasks: []*task{
+			t1, t2,
+		},
+	}
+
+	fmt.Println(lista.tasks[0])
+
+	lista.agregarALista(t3)
+
+	for i := 0; i < len(lista.tasks); i++ {
+		fmt.Println("index", i, "nombre", lista.tasks[i].nombre)
+	}
+
+	// la forma más utilizada y más elegante
+	for index, tarea := range lista.tasks {
+		fmt.Println("index", index, "nombre", tarea.nombre)
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			fmt.Println("con 'break' me voy")
+			break
+		}
+		fmt.Println(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			fmt.Println("con 'continue' me lo salteo y sigo")
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	lista.imprimirLista()
+	lista.tasks[0].marcarCompleta()
+	fmt.Println("Estas son las lista completas:")
+	lista.imprimirListaCompletado()
 
 }
